fcc3d560229207c8daeee72718f5b4822217ec80
Daniel Ploug Hall <danielploug@hotmail.com>
2026-01-18 14:36:52 +0100

    [refactor] Updated StartWithPythonNet such that its env variables that controls Venv and Version

--- PythonSerivce/StartWithPythonNet.sh
+++ PythonSerivce/StartWithPythonNet.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 # finds the libpython for an venv if given as the first argument
+# Meant to be used in the dockerfile of pythonserivce
 # Specifcally finds 3.13
+# Needs the enviormental Variables VENV and PYTHONVERISON
 
 . ./.venv/bin/activate
-pythonpath=$(dirname $(readlink -f $1/bin/python3.13))/../lib/libpython3.13.so
+pythonpath=$(dirname $(readlink -f $VENV/bin/python$PYTHONVERSION))/../lib/libpython$PYTHONVERSION.so
 export PYTHONNET_PYDLL=$(realpath $pythonpath)
-dotnet $2
+dotnet $1
